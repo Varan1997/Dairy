0feefab153ca289e4961a8e6ca1ab5cc9316c9ab
Raghu377326 <geddam.raghubabu@iitil.com>
2026-09-17 14:28:27 +0530

    commit

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,14 +1,16 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "/api",
+  baseURL: import.meta.env.VITE_API_URL || "/api",
 });
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
+
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
   }
+
   return config;
 });
 
@@ -19,6 +21,7 @@ api.interceptors.response.use(
       localStorage.removeItem("token");
       localStorage.removeItem("user");
     }
+
     return Promise.reject(error);
   }
 );
